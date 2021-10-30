6560a866c0d7564a1296ca18bc9c5203c5757a4b
Marisa Pholya <pholya_m@silpakorn.edu>
2021-10-30 21:06:13 +0700

    test commit

---
+++ lib/pages/test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TestPage extends StatefulWidget {
+  const TestPage({Key? key}) : super(key: key);
+
+  @override
+  _TestPageState createState() => _TestPageState();
+}
+
+class _TestPageState extends State<TestPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("TEST",style: TextStyle(
+          fontSize: 20.0,
+          color: Colors.brown,
+        ),),
+      ),
+    );
+  }
+}
